c2ba5d2b160cbfa8eb36e0175a9f65adcf11c92a
CaptainLuzik <dmitriy.luzanovsky@gmail.com>
2016-10-14 14:53:00 +0300

    Init

---
+++ bot/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Users, PaySystems, Enter, Withdrawal
+from datetime import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
+
+
+class UsersAdmin(admin.ModelAdmin):
+	fields = ['username', 'id_user', 'balance', 'date_reg']
+	list_display = ('username', 'id_user', 'balance', 'date_reg')
+
+admin.site.register(Users, UsersAdmin)
+
+class PaySystemsAdmin(admin.ModelAdmin):
+	fields = ['ps_name','ps_abbr','ps_purse']
+	list_display = ('ps_name','ps_abbr','ps_purse')
+
+admin.site.register(PaySystems, PaySystemsAdmin)
+
+class EnterAdmin(admin.ModelAdmin):
+	fields = ['username', 'en_id','en_ps','en_date','en_sum','en_status']
+	list_display = ('username', 'en_id','en_ps','en_date','en_sum','en_status')
+admin.site.register(Enter, EnterAdmin)
+
+class WithdrawalAdmin(admin.ModelAdmin):
+	fields = ['username', 'wd_id','wd_ps','wd_date','wd_sum','wd_status']
+	list_display = ('username', 'wd_id','wd_ps','wd_date','wd_sum','wd_status')
+admin.site.register(Withdrawal, WithdrawalAdmin)
+
+@receiver(post_save, sender = Enter)
+@receiver(post_save, sender = Users)
+def enter_balance(sender, instance, **kwargs):
+	status = instance.en_status
+	balance = instance.balance
+	if status == 2 and instance.en_sum:
+		user = instance.username
+		user = balance + instance.en_sum
+		user.save()
---
+++ bot/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from datetime import datetime
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=50, verbose_name='Имя пользователя')
+    id_user = models.IntegerField(verbose_name='ID-пользователя')
+    balance = models.FloatField(verbose_name='Баланс')
+    date_reg = models.DateTimeField(verbose_name='Дата регистрации')
+
+    class Meta:
+        verbose_name = 'Список пользователей'
+        verbose_name_plural = 'Список пользователей'
+
+    def __str__(self):
+        return self.username
+
+
+class PaySystems(models.Model):
+    ps_name = models.CharField(max_length=50, verbose_name='Платежная система')
+    ps_abbr = models.CharField(max_length=10, verbose_name='Аббревиатура')
+    ps_purse = models.CharField(max_length=30, verbose_name='Счет')
+
+    class Meta:
+        verbose_name = 'Платежные системы'
+        verbose_name_plural = 'Платежные системы'
+
+    def __str__(self):
+        return self.ps_name
+
+
+class Enter(models.Model):
+    STATUS = [
+        (1, 'В ожидании'),
+        (2, 'Обработано'),
+        (3, 'Отменено')
+    ]
+
+    username = models.ForeignKey(Users, verbose_name='Имя пользователя')
+    en_id = models.IntegerField(verbose_name='ID-транкзации')
+    en_ps = models.ForeignKey(PaySystems, verbose_name='Платежная система')
+    en_date = models.DateTimeField(verbose_name='Дата пополнения')
+    en_sum = models.FloatField(verbose_name='Сума пополнения')
+    en_status = models.IntegerField(
+        verbose_name='Статус платежа',
+        choices=STATUS
+    )
+
+    class Meta:
+        verbose_name = 'Пополнения счета'
+        verbose_name_plural = 'Пополнение счета'
+
+    def __str__(self):
+        return self.username.username
+
+
+class Withdrawal(models.Model):
+    STATUS = [
+        (1, 'В ожидании'),
+        (2, 'Обработано'),
+        (3, 'Отменено')
+    ]
+
+    username = models.ForeignKey(Users, verbose_name='Имя пользователя')
+    wd_id = models.IntegerField(verbose_name='ID-транкзации')
+    wd_ps = models.ForeignKey(PaySystems, verbose_name='Платежная система')
+    wd_date = models.DateTimeField(verbose_name='Дата пополнения')
+    wd_sum = models.FloatField(verbose_name='Сума пополнения')
+    wd_status = models.IntegerField(
+        verbose_name='Статус платежа',
+        choices=STATUS
+    )
+
+    class Meta:
+        verbose_name = 'Вывод средств'
+        verbose_name_plural = 'Вывод средств'
